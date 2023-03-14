ffa922488c22c9b25ea203e9bfe65081cfa50f98
Jhon Maya <mayamora.juan26@gmail.com>
2023-03-13 21:18:49 -0600

    Se agrego lista de articulos

--- fusionjs.js
+++ fusionjs.js
@@ -37,6 +37,22 @@ function toggleCarritoAside() {
     product.classList.toggle('inactive');
 }
 
+const producList = [];
+producList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+producList.push({
+    name: 'Celular',
+    price: 170,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+producList.push({
+    name: 'Computadora',
+    price: 250,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
 
 
 
