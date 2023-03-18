cf0cea5700fdf1e0892a8ce8b37bcc1fe9574a0e
Jhon Maya <mayamora.juan26@gmail.com>
2023-03-17 19:10:23 -0600

    Se modifico fusionjs.js

--- fusionjs.js
+++ fusionjs.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const product = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -54,6 +55,45 @@ producList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
+
+function renderProducts(arr){  
+  for (product of arr) {
+    const productCard = document.createElement('div');
+     productCard.classList.add('product-card');
+
+//product= {name, price, image} -> product.image
+    const productImag = document.createElement('img');
+    productImag.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure'); 
+    const productImagCart = document.createElement('img');
+    productImagCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImagCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);    
+    
+    productCard.appendChild(productImag);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+
+}
+}
 
 
 
